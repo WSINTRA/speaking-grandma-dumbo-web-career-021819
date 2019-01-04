c2c5c2ebdb53dfe165d9baa9d554085b120ba01b
William Robert Simpson <will.simpson85@gmail.com>
2019-01-04 20:36:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(some_string)
+def speak_to_grandma
   if some_string == some_string.upcase || !some_string == "I LOVE YOU GRANDMA!"
     sayBack = "NO, NOT SINCE 1938!"
   elsif some_string == "I LOVE YOU GRANDMA!"
